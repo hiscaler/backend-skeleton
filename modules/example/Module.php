292374665f78d1b57412eec289f1b1490c9ba3a9
hiscaler <hiscaler@gmail.com>
2017-12-07 17:09:58 +0800

    添加 example 测试模块

---
+++ modules/example/Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\modules\example;
+
+/**
+ * 测试模块
+ */
+class Module extends \yii\base\Module
+{
+
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\example\controllers';
+
+}
---
+++ modules/example/readme.txt
@@ -0,0 +1,5 @@
+name:模块示例
+author:hiscaler
+version:0.0.1
+description:仅做测试用，无实际用途
+url:http://www.example.com
