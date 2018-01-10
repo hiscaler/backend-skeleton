56ec26f9f53aea8dfaff618156fb37b10faa44dd
hiscaler <hiscaler@gmail.com>
2018-01-10 13:04:11 +0800

    admin: 完善附件上传设置管理表单

--- modules/admin/views/file-upload-configs/_form.php
+++ modules/admin/views/file-upload-configs/_form.php
@@ -8,17 +8,15 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\FileUploadConfig */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-
 <div class="form-outside">
     <div class="file-upload-config-form form">
-
         <?php $form = ActiveForm::begin(); ?>
 
         <?= $form->field($model, 'type')->dropDownList(FileUploadConfig::typeOptions()) ?>
 
         <?= $form->field($model, 'model_attribute')->dropDownList(FileUploadConfig::modelAttributeOptions(), ['prompt' => '']) ?>
 
-        <?= $form->field($model, 'extensions')->textInput(['maxlength' => 255]) ?>
+        <?= $form->field($model, 'extensions')->textInput(['maxlength' => true, 'placeholder' => '*']) ?>
 
         <?=
         $form->field($model, 'min_size', [
@@ -34,21 +32,34 @@ use yii\widgets\ActiveForm;
 
         <?=
         $form->field($model, 'thumb_width', [
-            'template' => "{label}\n{input} PX\n{hint}\n{error}",
+            'template' => "{label}\n{input} px\n{hint}\n{error}",
         ])->textInput(['class' => 'g-text-number'])
         ?>
 
         <?=
         $form->field($model, 'thumb_height', [
-            'template' => "{label}\n{input} PX\n{hint}\n{error}",
+            'template' => "{label}\n{input} px\n{hint}\n{error}",
         ])->textInput(['class' => 'g-text-number'])
         ?>
-
         <div class="form-group buttons">
             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
-
         <?php ActiveForm::end(); ?>
-
     </div>
 </div>
+<?php \app\modules\admin\components\JsBlock::begin() ?>
+<script type="text/javascript">
+    $(function () {
+        $('#fileuploadconfig-type').change(function () {
+            var type = $(this).val();
+            if (!$('#fileuploadconfig-extensions').val()) {
+                if (type == 0) {
+                   $('#fileuploadconfig-extensions').val('zip,doc,docx,xls,xlsx,pdf');
+                } else if (type == 1) {
+                    $('#fileuploadconfig-extensions').val('jpg,jpeg,png,gif');
+                }
+            }
+        });
+    });
+</script>
+<?php \app\modules\admin\components\JsBlock::end() ?>
