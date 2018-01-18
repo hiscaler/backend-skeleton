3f07d776048095bd7f6e7f5e5622f3fdc8748b9d
hiscaler <hiscaler@gmail.com>
2018-01-18 21:16:54 +0800

    api: 完善 wxapp 接口，并添加 wxapp/payment 支付接口

--- modules/api/controllers/WxappController.php
+++ modules/api/controllers/WxappController.php
@@ -2,13 +2,17 @@
 
 namespace app\modules\api\controllers;
 
-use app\models\Meta;
 use app\modules\api\models\Constant;
 use app\modules\api\models\Member;
+use Overtrue\Wechat\Payment;
+use Overtrue\Wechat\Payment\Business;
+use Overtrue\Wechat\Payment\Order;
+use Overtrue\Wechat\Payment\UnifiedOrder;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\base\InvalidParamException;
 use yii\base\InvalidValueException;
+use yii\web\NotFoundHttpException;
 
 /**
  * 小程序认证处理
@@ -63,7 +67,7 @@ class WxappController extends Controller
         $token = file_get_contents($url);
         $token && $token = json_decode($token, true);
         if (empty($token) || isset($token['errcode'])) {
-            throw new InvalidValueException('微信接口访问出错（' . $token['errmsg'] . '）。');
+            throw new InvalidValueException("微信接口访问出错（{$token['errmsg']}）。");
         }
         $openid = $token['openid'];
         if (empty($openid)) {
@@ -71,7 +75,7 @@ class WxappController extends Controller
         }
 
         $now = time();
-        $accessTokenValue = 'wxapp.' . md5($openid . $token['session_key']) . '.' . ($now + 7200);
+        $accessToken = 'wxapp.' . md5($openid . $token['session_key']) . '.' . ($now + 7200);
         $db = \Yii::$app->getDb();
         $memberId = $db->createCommand('SELECT [[member_id]] FROM {{%wechat_member}} WHERE [[openid]] = :openid', [':openid' => $openid])->queryScalar();
         $nickname = preg_replace('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $info["nickName"]);
@@ -82,7 +86,7 @@ class WxappController extends Controller
             $member->nickname = $nickname;
             $member->last_login_time = $now;
             $member->login_count += 1;
-            $member->access_token = $accessTokenValue;
+            $member->access_token = $accessToken;
             $isNewRecord = false;
         } else {
             // 添加新会员
@@ -93,12 +97,11 @@ class WxappController extends Controller
             $member->username = substr(md5($openid), -20);
             $member->last_login_time = $now;
             $member->login_count = 1;
-            $member->access_token = $accessTokenValue;
+            $member->access_token = $accessToken;
             $isNewRecord = true;
         }
         if ($member->validate() && $member->save()) {
             $wechatMember = [
-                'subscribe' => Constant::BOOLEAN_FALSE,
                 'openid' => $openid,
                 'nickname' => $nickname,
                 'sex' => $info['gender'],
@@ -107,21 +110,20 @@ class WxappController extends Controller
                 'city' => isset($info['city']) ? $info['city'] : null,
                 'language' => isset($info['language']) ? $info['language'] : null,
                 'headimgurl' => $info['avatarUrl'],
-                'subscribe_time' => null,
                 'unionid' => isset($info['union_id']) ? $info['union_id'] : null,
             ];
             if ($isNewRecord) {
                 $wechatMember['member_id'] = $member->id;
+                $wechatMember['subscribe'] = Constant::BOOLEAN_FALSE;
+                $wechatMember['subscribe_time'] = null;
                 $db->createCommand()->insert('{{%wechat_member}}', $wechatMember)->execute();
             } else {
                 $db->createCommand()->update('{{%wechat_member}}', $wechatMember, ['openid' => $openid])->execute();
             }
 
-            Meta::updateValue('wechat_member', 'wxapp_session_key', $member->id, $accessTokenValue);
-            Meta::updateValue('wechat_member', 'wxapp_session_time', $member->id, $now + 86400);
-
             return [
-                'session' => $accessTokenValue,
+                'session' => $accessToken,
+                'openid' => $openid,
             ];
         } else {
             Yii::$app->getResponse()->setStatusCode(400);
@@ -144,14 +146,66 @@ class WxappController extends Controller
         }
         $accessToken = Yii::$app->getDb()->createCommand('SELECT [[access_token]] FROM {{%member}} WHERE [[access_token]] = :accessToken', [':accessToken' => $session])->queryScalar();
         if ($accessToken) {
-            list(, , $timestamp) = explode(',', $accessToken['access_token']);
+            $t = explode('.', $accessToken);
+            $timestamp = isset($t[2]) ? $t[2] : null;
 
             return [
-                'valid' => $timestamp <= time()
+                'valid' => $timestamp && $timestamp > time()
             ];
         } else {
             throw new NotFoundHttpException('Member data is not exists.');
         }
+    }
+
+    /**
+     * @return array|string
+     * @throws InvalidConfigException
+     * @throws \yii\db\Exception
+     */
+    public function actionPayment()
+    {
+        $options = isset(Yii::$app->params['wechat']) ? Yii::$app->params['wechat'] : [];
+        if (!isset($options['appid'], $options['secret'], $options['mch_id'], $options['mch_key'])) {
+            throw new InvalidConfigException('无效的微信公众号配置。');
+        }
+        $request = Yii::$app->getRequest();
+        $body = trim($request->post('body', ''));
+        $outTradeNo = trim($request->post('out_trade_no'));
+        $outTradeNo || $outTradeNo = date('YmdHis') . mt_rand(1000, 9999);
+        $totalFee = (int) $request->post('total_fee');
+        if (!$totalFee) {
+            throw new InvalidParamException('无效的 total_fee 参数值。');
+        }
+        $openid = trim($request->post('openid'));
+        if (!$openid) {
+            throw new InvalidParamException('无效的 openid 参数值。');
+        }
+        $exist = \Yii::$app->getDb()->createCommand('SELECT COUNT(*) FROM {{%wechat_member}} WHERE [[openid]] = :openid', [':openid' => $openid])->queryScalar();
+        if (!$exist) {
+            throw new InvalidParamException("openid $openid 不存在。");
+        }
+        $notifyUrl = $request->post('notify_url');
+        if (!$notifyUrl || !filter_var($notifyUrl, FILTER_VALIDATE_URL)) {
+            throw new InvalidParamException('无效的 notify_url 参数值。');
+        }
+        $order = new Order();
+        $order->body = $body;
+        $order->out_trade_no = $outTradeNo;
+        $order->total_fee = $totalFee;
+        $order->openid = $openid;
+        $order->notify_url = $notifyUrl;
+
+        $unifiedOrder = new UnifiedOrder(new Business($options['appid'], $options['secret'], $options['mch_id'], $options['mch_key']), $order);
+        $payment = new Payment($unifiedOrder);
+
+        // 创建订单
+        $columns = $order->toArray();
+        unset($columns['notify_url']);
+        $columns['create_time'] = time();
+        $columns['status'] = \app\modules\admin\modules\wxpay\models\Order::STATUS_PENDING;
+        \Yii::$app->getDb()->createCommand()->insert('{{%wx_order}}', $columns)->execute();
+
+        return $payment->getConfigJssdk(false);
     }
 
 }
