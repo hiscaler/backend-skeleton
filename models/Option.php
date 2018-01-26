2819945cb8aa6473d47c217cc6a3614af7e60220
hiscaler <hiscaler@gmail.com>
2018-01-26 16:31:33 +0800

    app\models\Option 添加 tables，models 函数，用于获取数据表和模型数据

--- models/Option.php
+++ models/Option.php
@@ -4,6 +4,7 @@ namespace app\models;
 
 use Yii;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Inflector;
 
 class Option
 {
@@ -75,6 +76,56 @@ class Option
             Constant::SEX_MALE => '男',
             Constant::SEX_FEMALE => '女',
         ];
+    }
+
+    /**
+     * 获取所有表名称
+     *
+     * @return string[]
+     * @throws \yii\base\NotSupportedException
+     */
+    public static function tables()
+    {
+        return Yii::$app->getDb()->getSchema()->getTableNames('', true);
+    }
+
+    /**
+     * 获取所有数据模型
+     *
+     * @return array
+     * @throws \yii\base\NotSupportedException
+     */
+    public static function models()
+    {
+        $models = [];
+        $tablePrefix = \Yii::$app->getDb()->tablePrefix;
+        $coreTables = ['category', 'entity_label', 'file_upload_config', 'grid_column_config', 'label', 'lookup', 'member', 'meta', 'meta_validator', 'meta_value', 'migration', 'module', 'user', 'user_auth_category', 'user_credit_log', 'user_group', 'user_login_log', 'wechat_member'];
+        $path = Yii::getAlias('@app');
+        foreach (self::tables() as $table) {
+            $table = str_replace($tablePrefix, '', $table);
+            if ($table == 'migration') {
+                continue;
+            }
+            $modelName = Inflector::id2camel($table, '_');
+            if (in_array($table, $coreTables) && file_exists($path . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . $modelName . '.php')) {
+                $models[$table] = "app\\model\\$modelName";
+            } else {
+                $index = stripos($table, '_');
+                if ($index === false) {
+                    $moduleName = $modelName = $table;
+                } else {
+                    $moduleName = substr($table, 0, $index);
+                    $modelName = substr($table, $index + 1);
+                }
+                $moduleName = strtolower($moduleName);
+                $modelName = Inflector::id2camel($modelName, '_');
+                if ($moduleName !== false && file_exists($path . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $moduleName . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . $modelName . '.php')) {
+                    $models[$table] = "app\\modules\\$moduleName\\models\\$modelName";
+                }
+            }
+        }
+
+        return $models;
     }
 
 }
