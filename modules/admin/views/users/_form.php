e14ea16cab1dd1d3bcc518cc27a1a7dd5d35e20a
hiscaler <hiscaler@gmail.com>
2018-01-30 10:08:53 +0800

    admin: 系统管理员支持自定义表单内容处理

--- modules/admin/views/users/_form.php
+++ modules/admin/views/users/_form.php
@@ -8,29 +8,36 @@ use app\models\User;
 /* @var $model app\models\User */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-<div class="form-outside">
-    <div class="form user-form">
-        <?php $form = ActiveForm::begin(); ?>
-
-        <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-
-        <?php if ($model->isNewRecord): ?>
-            <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
-
-            <?= $form->field($model, 'confirm_password')->passwordInput(['maxlength' => true]) ?>
+<div>
+    <ul class="tabs-common">
+        <li class="active"><a href="javascript:;" data-toggle="tab-panel-basic">基本数据</a></li>
+        <?php if ($dynamicModel->getMetaOptions()): ?>
+            <li><a href="javascript:;" data-toggle="tab-panel-metas">扩展属性</a></li>
         <?php endif; ?>
+    </ul>
+    <div class="panels form">
+        <?php $form = ActiveForm::begin(); ?>
+        <div class="tab-panel" id="tab-panel-basic">
+            <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+            <?php if ($model->isNewRecord): ?>
+                <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'nickname')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'confirm_password')->passwordInput(['maxlength' => true]) ?>
+            <?php endif; ?>
 
-        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nickname')->textInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'role')->dropDownList(User::roleOptions(), ['prompt' => '']) ?>
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'status')->dropDownList(User::statusOptions(), ['prompt' => '']) ?>
+            <?= $form->field($model, 'role')->dropDownList(User::roleOptions(), ['prompt' => '']) ?>
 
-        <?php foreach ($dynamicModel->getMetaOptions() as $metaItem): ?>
-            <?= $form->field($dynamicModel, $metaItem['key'])->$metaItem['input_type'](['value' => $metaItem['value']])->label($metaItem['label']) ?>
-        <?php endforeach; ?>
+            <?= $form->field($model, 'status')->dropDownList(User::statusOptions(), ['prompt' => '']) ?>
+        </div>
+        <div class="tab-panel" id="tab-panel-metas" style="display: none">
+            <?php foreach ($dynamicModel->getMetaOptions() as $metaItem): ?>
+                <?= $form->field($dynamicModel, $metaItem['key'])->{$metaItem['input_type']}(['value' => $metaItem['value']])->label($metaItem['label']) ?>
+            <?php endforeach; ?>
+        </div>
         <div class="form-group buttons">
             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
