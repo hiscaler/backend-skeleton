9f08c7e4feda8cf65d887a3a7141870959869be5
hiscaler <hiscaler@gmail.com>
2018-02-01 16:18:29 +0800

    ...

--- messages/zh-CN/model.php
+++ messages/zh-CN/model.php
@@ -4,27 +4,17 @@
  * Model
  */
 return [
-    'Ad' => '广告',
-    'Ad Space' => '广告位',
     'Label' => '推送位',
-    'Base Code' => '基础代码',
     'Category' => '分类',
-    'Email User' => '邮箱用户',
-    'Email User Group' => '邮箱用户分组',
     'File Upload Config' => '文件上传设定',
-    'Group Option' => '分组设定',
     'Grid Column Config' => '显示栏位',
-    'IP Access Rule' => 'IP 访问规则',
     'Lookup' => '常规设定',
     'Member' => '会员',
-    'Meta' => '自定义表单',
+    'Meta' => '扩展属性',
     'Module' => '模块管理',
-    'Tag' => '标签',
     'User' => '用户',
     'User Group' => '用户分组',
     'User Credit Log' => '用户积分',
     'User Login Log' => '用户登录日志',
-    'Workflow Rule' => '流程规则',
-    'Workflow Rule Definition' => '流程规则定义',
     'Wechat Member' => '微信会员',
 ];
