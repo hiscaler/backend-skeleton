7732703bf56b310688800100ffa70a5fe9da03d8
hiscaler <hiscaler@gmail.com>
2018-02-01 17:36:42 +0800

    admin: 完善 link 子模块分类选择

--- modules/admin/modules/link/views/default/_form.php
+++ modules/admin/modules/link/views/default/_form.php
@@ -15,7 +15,7 @@ use yii\widgets\ActiveForm;
             ]
         ]); ?>
 
-        <?= $form->field($model, 'category_id')->dropDownList(\app\models\Category::tree('link'), ['prompt' => '']) ?>
+        <?= $form->field($model, 'category_id')->dropDownList(\app\models\Category::tree('link', \app\models\Category::RETURN_TYPE_PRIVATE), ['prompt' => '']) ?>
 
         <?= $form->field($model, 'type')->dropDownList(\app\modules\admin\modules\link\models\Link::typeOptions()) ?>
 
