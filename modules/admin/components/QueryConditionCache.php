36afeaac41415fef45985f9a22fe52f0bf433e3c
hiscaler <hiscaler@gmail.com>
2018-02-03 22:36:15 +0800

    admin: 添加 QueryConditionCache 类，用于保存查询条件供二次使用

---
+++ modules/admin/components/QueryConditionCache.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\modules\admin\components;
+
+use Yii;
+
+/**
+ * Query Condition CacheClass, Use CCache class to set, get or remove query conditions.
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class QueryConditionCache
+{
+
+    /**
+     * Set to cache
+     *
+     * @param string $key
+     * @param mixed $conditions
+     * @return mixed
+     */
+    public static function set($key, $conditions)
+    {
+        if (!empty($key)) {
+            Yii::$app->getCache()->set(strtolower(self::parseKey($key)), $conditions);
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Get from cache
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public static function get($key)
+    {
+        if (!empty($key)) {
+            $value = Yii::$app->getCache()->get(self::parseKey($key));
+
+            return ($value != false) ? $value : null;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Remove it by key
+     *
+     * @param string $key
+     */
+    public static function remove($key)
+    {
+        if (!empty($key)) {
+            Yii::$app->getCache()->delete(self::parseKey($key));
+        }
+    }
+
+    /**
+     * Parse key, if key is not empty, then add 'query.condition.' prefix
+     *
+     * @param string $key
+     * @return string
+     */
+    private static function parseKey($key)
+    {
+        return strtolower(!empty($key) ? "query.condition.{$key}" : $key);
+    }
+
+}
