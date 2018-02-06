dc00ad2eb748c33f9b53eba05859458dce41a0ba
hiscaler <hiscaler@gmail.com>
2018-02-06 13:31:58 +0800

    admin: 重构帮助中心数据展示

--- modules/admin/controllers/HelpController.php
+++ modules/admin/controllers/HelpController.php
@@ -4,16 +4,57 @@ namespace app\modules\admin\controllers;
 
 use cebe\markdown\GithubMarkdown;
 use Yii;
+use yii\helpers\FileHelper;
 use yii\web\NotFoundHttpException;
 
 /**
- * Class HelpController
+ * 帮助中心
  *
  * @package app\modules\admin\controllers
  * @author hiscaler <hiscaler@gmail.com>
  */
 class HelpController extends Controller
 {
+
+    private function getDocs()
+    {
+        $docs = [];
+        $markdown = new GithubMarkdown();
+        $appPath = Yii::getAlias('@app');
+        $files = FileHelper::findFiles($appPath . '/docs');
+        foreach (\app\models\Module::getModules() as $alias => $name) {
+            $path = $appPath . "/modules/api/modules/{$alias}/docs";
+            if (file_exists($path)) {
+                $files = array_merge($files, FileHelper::findFiles($path));
+            }
+        }
+        foreach ($files as $file) {
+            $content = file_get_contents($file);
+            if ($content === false) {
+                continue;
+            }
+            $filename = basename($file, '.md');
+            preg_match("/modules\/api\/modules\/(\w.*)\/docs/", $file, $matches);
+            if ($matches) {
+                $filename = $matches[1] . ".$filename";
+            }
+            $content = $markdown->parse($content);
+            preg_match('/<h1>(.*)<\/h1>?.*/', $content, $matches);
+            if ($matches) {
+                $title = $matches[1];
+            } else {
+                $title = $filename;
+            }
+            $navigation = [];
+            $docs[$filename] = [
+                'title' => $title,
+                'content' => $content,
+                'navigation' => $navigation
+            ];
+        }
+
+        return $docs;
+    }
 
     /**
      * 文档查看
@@ -24,26 +65,23 @@ class HelpController extends Controller
      */
     public function actionIndex($file = 'about')
     {
-        $filePath = Yii::getAlias('@app/docs/' . strtolower($file) . '.md');
-        if (file_exists($filePath)) {
-            $content = file_get_contents($filePath);
-            $markdown = new GithubMarkdown();
-
-            $this->layout = 'help';
-
-            $content = $markdown->parse($content);
-            preg_match('/<h1>(.*)<\/h1>?.*/', $content, $matches);
-            if ($matches) {
-                $title = $matches[1];
-            } else {
-                $title = 'ERROR';
+        $docs = $this->getDocs();
+        if (isset($docs[$file])) {
+            $this->layout = false;
+            $sections = [];
+            foreach ($docs as $key => $doc) {
+                $sections[] = [
+                    'label' => $doc['title'],
+                    'url' => ['help/index', 'file' => $key],
+                ];
             }
 
             return $this->render('index.php', [
-                'title' => $title,
-                'body' => $content,
+                'sections' => $sections,
+                'doc' => $docs[$file]
             ]);
         }
+
         throw new NotFoundHttpException("$file 文档不存在。");
     }
 }
--- modules/admin/views/help/index.php
+++ modules/admin/views/help/index.php
@@ -1,8 +1,56 @@
 <?php
 
-$this->title = $title;
+use yii\widgets\Breadcrumbs;
+
+$this->title = $doc['title'];
 $this->params['breadcrumbs'][] = ['label' => '帮助中心', 'url' => ['help/index']];
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerCssFile(Yii::$app->getRequest()->getBaseUrl() . '/admin/css/help.css');
+$this->beginContent('@app/modules/admin/views/layouts/base.php');
 ?>
-<?= $body ?>
-<?php $this->registerCssFile(Yii::$app->getRequest()->getBaseUrl() . '/admin/css/help.css') ?>
+    <div class="layout grid-s6m0e0">
+        <div class="col-main">
+            <div class="main-wrap">
+                <?php
+                if (isset($this->params['breadcrumbs']) && $this->params['breadcrumbs']) {
+                    echo '<div class="breadcrumbs clearfix">';
+                    echo Breadcrumbs::widget(
+                        [
+                            'itemTemplate' => "<li>{link}<i>&raquo;</i></li>",
+                            'homeLink' => [
+                                'label' => Yii::t('app', 'Homepage'),
+                                'url' => ['/admin/default/index'],
+                            ],
+                            'links' => $this->params['breadcrumbs'],
+                        ]);
+                    echo '</div>';
+                }
+                ?>
+                <div class="container">
+                    <div class="inner">
+                        <div id="help-body-render">
+                            <?= $doc['content'] ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sub">
+            <div class="control-panel">
+                <div class="inner">
+                    <div class="title shortcut">帮助中心</div>
+                    <div class="shortcuts">
+                        <?= yii\widgets\Menu::widget([
+                            'items' => $sections,
+                            'itemOptions' => ['class' => 'clearfix'],
+                            'firstItemCssClass' => 'first',
+                            'lastItemCssClass' => 'last',
+                            'activateParents' => true,
+                        ]);
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $this->endContent(); ?>
