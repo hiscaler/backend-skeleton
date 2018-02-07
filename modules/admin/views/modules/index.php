33204581887789be6fea4d5f76b8aee3197d3862
hiscaler <hiscaler@gmail.com>
2018-02-07 13:45:26 +0800

    admin: 优化扩展模块安装和下载操作

--- modules/admin/views/modules/index.php
+++ modules/admin/views/modules/index.php
@@ -14,53 +14,64 @@ $this->params['breadcrumbs'][] = $this->title;
     </ul>
     <div class="panels">
         <div id="panel-installed" class="tab-panel">
-            <ul>
-                <?php foreach ($installedModules as $i => $module): ?>
-                    <li id="module-<?= $module['alias'] ?>" class="widget-module">
-                        <div class="hd">
-                            <em><?= $module['name'] ?></em>
-                            <span class="icon"><?= Html::img($module['icon'], ['src' => $module['name']]) ?></span>
-                            <span class="buttons">
-                                <?= Html::a(Yii::t('module', 'Uninstall'), ['uninstall', 'alias' => $module['alias']], ['class' => 'uninstall', 'data-key' => $module['alias'], 'data-url' => \yii\helpers\Url::toRoute(['install', 'alias' => $module['alias']])]) ?>
-                                <?= Html::a(Yii::t('module', 'Upgrade'), ['upgrade', 'alias' => $module['alias']], ['class' => 'upgrade', 'data-key' => $module['alias'], 'data-name' => $module['name'], 'data-url' => \yii\helpers\Url::toRoute(['upgrade', 'alias' => $module['alias']])]) ?>
-                            </span>
-                        </div>
-                        <div class="bd">
-                            <p class="misc">
-                                <span><?= Yii::t('module', 'Author') ?>：<?= $module['author'] ?></span>
-                                <span><?= Yii::t('module', 'Version') ?>：<?= $module['version'] ?></span>
-                                <span><?= Yii::t('module', 'Url') ?>：<a href="<?= $module['url'] ?>" target="_blank"><?= $module['url'] ?></a></span>
-                            </p>
-                            <p class="description">
-                                <?= $module['description'] ?>
-                            </p>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <?php if ($installedModules): ?>
+                <ul>
+                    <?php foreach ($installedModules as $i => $module): ?>
+                        <li id="module-<?= $module['alias'] ?>" class="widget-module">
+                            <div class="hd">
+                                <em><?= $module['name'] ?></em>
+                                <span class="icon"><?= Html::img($module['icon'], ['src' => $module['name']]) ?></span>
+                                <span class="buttons">
+                                    <?= Html::a(Yii::t('module', 'Uninstall'), ['uninstall', 'alias' => $module['alias']], ['class' => 'uninstall', 'data-key' => $module['alias'], 'data-url' => \yii\helpers\Url::toRoute(['install', 'alias' => $module['alias']])]) ?>
+                                    <?= Html::a(Yii::t('module', 'Upgrade'), ['upgrade', 'alias' => $module['alias']], ['class' => 'upgrade', 'data-key' => $module['alias'], 'data-name' => $module['name'], 'data-url' => \yii\helpers\Url::toRoute(['upgrade', 'alias' => $module['alias']])]) ?>
+                                </span>
+                            </div>
+                            <div class="bd">
+                                <p class="misc">
+                                    <span><?= Yii::t('module', 'Author') ?>：<?= $module['author'] ?></span>
+                                    <span><?= Yii::t('module', 'Version') ?>：<?= $module['version'] ?></span>
+                                    <span><?= Yii::t('module', 'Url') ?>：<a href="<?= $module['url'] ?>" target="_blank"><?= $module['url'] ?></a></span>
+                                </p>
+                                <p class="description">
+                                    <?= $module['description'] ?>
+                                </p>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <div class="notice">您还没有安装任何模块。</div>
+            <?php endif; ?>
         </div>
         <div id="panel-notinstalled" class="tab-panel" style="display: none">
-            <ul>
-                <?php foreach ($notInstalledModules as $i => $module): ?>
-                    <li id="module-<?= $module['alias'] ?>" class="widget-module">
-                        <div class="hd">
-                            <em><?= $module['name'] ?></em>
-                            <span class="icon"><?= Html::img($module['icon'], ['src' => $module['name']]) ?></span>
-                            <span class="buttons"><?= Html::a(Yii::t('module', 'Install'), ['install', 'alias' => $module['alias']], ['class' => 'install', 'data-key' => $module['alias'], 'data-url' => \yii\helpers\Url::toRoute(['uninstall', 'alias' => $module['alias']])]) ?></span>
-                        </div>
-                        <div class="bd">
-                            <p class="misc">
-                                <span><?= Yii::t('module', 'Author') ?>：<?= $module['author'] ?></span>
-                                <span><?= Yii::t('module', 'Version') ?>：<?= $module['version'] ?></span>
-                                <span><?= Yii::t('module', 'Url') ?>：<a href="<?= $module['url'] ?>" target="_blank"><?= $module['url'] ?></a></span>
-                            </p>
-                            <p class="description">
-                                <?= $module['description'] ?>
-                            </p>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <?php if ($notInstalledModules): ?>
+                <ul>
+                    <?php foreach ($notInstalledModules as $i => $module): ?>
+                        <li id="module-<?= $module['alias'] ?>" class="widget-module">
+                            <div class="hd">
+                                <em><?= $module['name'] ?></em>
+                                <span class="icon"><?= Html::img($module['icon'], ['src' => $module['name']]) ?></span>
+                                <span class="buttons">
+                                    <?= Html::a(Yii::t('module', 'Install'), ['install', 'alias' => $module['alias']], ['class' => 'install', 'data-key' => $module['alias'], 'data-url' => \yii\helpers\Url::toRoute(['uninstall', 'alias' => $module['alias']])]) ?>
+                                    <?= Html::a(Yii::t('module', 'Upgrade'), ['upgrade', 'alias' => $module['alias']], ['class' => 'upgrade', 'data-key' => $module['alias'], 'data-name' => $module['name'], 'data-url' => \yii\helpers\Url::toRoute(['upgrade', 'alias' => $module['alias']]), 'style' => 'display: none']) ?>
+                                </span>
+                            </div>
+                            <div class="bd">
+                                <p class="misc">
+                                    <span><?= Yii::t('module', 'Author') ?>：<?= $module['author'] ?></span>
+                                    <span><?= Yii::t('module', 'Version') ?>：<?= $module['version'] ?></span>
+                                    <span><?= Yii::t('module', 'Url') ?>：<a href="<?= $module['url'] ?>" target="_blank"><?= $module['url'] ?></a></span>
+                                </p>
+                                <p class="description">
+                                    <?= $module['description'] ?>
+                                </p>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <div class="notice">暂无待安装模块</div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
@@ -75,12 +86,7 @@ $this->params['breadcrumbs'][] = $this->title;
             if ($t.hasClass('install')) {
                 _doInstallUninstall($t);
             } else {
-                var droptableMessage = '';
-                <?php
-                if (isset(Yii::$app->params['uninstall.module.after.droptable']) && Yii::$app->params['uninstall.module.after.droptable'] === true) {
-                    echo "droptableMessage = '卸载后将同步删除模块相关数据表！！！';";
-                }
-                ?>
+                var droptableMessage = '<?= (isset(Yii::$app->params['uninstall.module.after.droptable']) && Yii::$app->params['uninstall.module.after.droptable'] === true) ? '卸载后将同步删除模块相关数据表！！！' : '' ?>';
                 layer.confirm('您是否确定卸载"' + $t.parent().parent().find('em').html() + '"模块？' + droptableMessage, {
                     btn: ['确定卸载', '取消'] //按钮
                 }, function (index) {
@@ -115,9 +121,27 @@ $this->params['breadcrumbs'][] = $this->title;
                         var $tmp = $obj.clone(true, true);
                         $obj.remove();
                         if (isInstall) {
+                            if ($('#panel-installed').find('ul').length === 0) {
+                                $('#panel-installed .notice').remove();
+                                $('#panel-installed').append('<ul></ul>');
+                            }
+                            $tmp.find('.upgrade').show();
                             $('#panel-installed ul').append($tmp);
+                            if ($('#panel-notinstalled').find('li').length === 0) {
+                                $('#panel-notinstalled').append('<div class="notice">所有模块都已经安装完毕。</div>');
+                            }
                         } else {
+                            if ($('#panel-notinstalled').find('ul').length === 0) {
+                                $('#panel-notinstalled .notice').remove();
+                                $('#panel-notinstalled').append('<ul></ul>');
+                            }
+                            $tmp.find('.upgrade').hide();
                             $('#panel-notinstalled ul').append($tmp);
+
+                            if ($('#panel-installed').find('li').length === 0) {
+                                $('#panel-installed').append('<div class="notice">所有模块都已经卸载完毕。</div>');
+                            }
+
                             $('#control-panel-module-' + $t.attr('data-key')).parent().remove();
                         }
                     } else {
@@ -140,7 +164,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 },
                 success: function (response) {
                     if (response.success) {
-                        layer.alert($t.attr('data-name') + '模块更新成功。', {icon: 1});
+                        layer.alert('"' + $t.attr('data-name') + '" 模块更新成功。', {icon: 1});
                     } else {
                         layer.alert(response.error.message);
                     }
