c48a98bb5f6bf67b2b67a6e05d8803600740c1cc
hiscaler <hiscaler@gmail.com>
2018-02-07 11:24:53 +0800

    admin: 完善错误页面

--- modules/admin/views/default/error.php
+++ modules/admin/views/default/error.php
@@ -1,19 +1,37 @@
 <?php
+$this->context->layout = false;
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 
 /* @var $exception Exception */
 
+/* @var $this \yii\web\View */
+
+/* @var $content string */
+
 use yii\helpers\Html;
 
-$this->title = $name;
-
-$this->params['breadcrumbs'][] = Yii::t('app', 'Prompt Message');
+app\modules\admin\assets\AppAsset::register($this);
 ?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $name . ' - ' . \app\models\Lookup::getValue('custom.site.name') ?: Yii::$app->name ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
 <div class="site-error">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= $name ?></h1>
     <div class="message">
         <?= nl2br(Html::encode($message)) ?>
     </div>
 </div>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
