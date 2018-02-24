f461c27f2482a514c59439c2db9815ed1f38642f
hiscaler <hiscaler@gmail.com>
2018-02-25 01:23:56 +0800

    admin.rbac: 移除模块中 i18n 设置

--- modules/admin/modules/rbac/Module.php
+++ modules/admin/modules/rbac/Module.php
@@ -23,15 +23,7 @@ class Module extends \app\modules\admin\Module
             'formatter' => [
                 'class' => 'app\modules\admin\modules\rbac\extensions\Formatter',
             ],
-            'i18n' => [
-                'class' => 'yii\i18n\I18N',
-                'translations' => [
-                    'rbac' => [
-                        'class' => '\yii\i18n\PhpMessageSource',
-                        'basePath' => __DIR__ . '/messages',
-                    ],
-                ],
-            ],
         ]);
     }
+
 }
