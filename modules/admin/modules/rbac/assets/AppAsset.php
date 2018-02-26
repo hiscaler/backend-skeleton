b5ba9996d1b319c23a853874fddfdf64a6a629c3
hiscaler <hiscaler@gmail.com>
2018-02-26 17:39:12 +0800

    admin.rbac: htmml CSS 内容移到文件中

---
+++ modules/admin/modules/rbac/assets/AppAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\admin\modules\rbac\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+
+    public $sourcePath = '@app/modules/admin/modules/rbac/statics';
+    public $css = [
+        'css/application.min.css',
+    ];
+
+}
