0d828fd3b33557720db93ea490bf51eef19b5ddd
hiscaler <hiscaler@gmail.com>
2018-05-07 21:19:43 +0800

    ...

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -8,6 +8,7 @@
 
 namespace app\controllers;
 
+use app\models\Constant;
 use app\models\Member;
 use EasyWeChat\Foundation\Application;
 use Yii;
@@ -18,6 +19,11 @@ class WechatController extends Controller
 
     public function actionAuth($redirectUrl = null, $redirect = 1)
     {
+        if ($redirectUrl) {
+            $redirectUrl = urldecode($redirectUrl);
+        } else {
+            $redirectUrl = Yii::$app->getRequest()->getHostInfo();
+        }
         $webUser = Yii::$app->getUser();
         $db = Yii::$app->getDb();
         if ($webUser->isGuest) {
@@ -27,7 +33,13 @@ class WechatController extends Controller
                 $oauth = $application->oauth;
                 $request = Yii::$app->getRequest();
                 $url = $request->getHostInfo() . $request->getUrl();
-                $url .= '&redirect=0';
+
+                if (strpos($url, '?') === false) {
+                    $url .= '?';
+                } else {
+                    $url .= '&';
+                }
+                $url .= 'redirect=0';
                 $oauth->setRedirectUrl($url);
                 $url = $oauth->redirect()->getTargetUrl();
                 header("Location: $url");
@@ -40,10 +52,12 @@ class WechatController extends Controller
                 $openid = $userOriginal['openid'];
                 $memberId = $db->createCommand('SELECT [[member_id]] FROM {{%wechat_member}} WHERE [[openid]] = :openid', [':openid' => $openid])->queryScalar();
                 if (!$memberId) {
+                    $nickname = preg_replace('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $userOriginal["nickname"]);
                     $member = new Member();
                     $member->setPassword($openid);
-                    $member->username = $userOriginal['nickname'];
-                    $member->nickname = $userOriginal['nickname'];
+                    $count = $db->createCommand('SELECT COUNT(*) FROM {{%member}}')->queryScalar();
+                    $member->username = 'wx' . ($count + 1);
+                    $member->nickname = $nickname;
                     $member->avatar = $userOriginal['headimgurl'];
                     $member->status = Member::STATUS_ACTIVE;
                     if ($member->save()) {
@@ -52,7 +66,7 @@ class WechatController extends Controller
                             'member_id' => $memberId,
                             'subscribe' => Constant::BOOLEAN_TRUE,
                             'openid' => $openid,
-                            'nickname' => $userOriginal['nickname'],
+                            'nickname' => $nickname,
                             'sex' => $userOriginal['sex'],
                             'country' => $userOriginal['country'],
                             'province' => $userOriginal['province'],
@@ -72,19 +86,17 @@ class WechatController extends Controller
                     $accessTokenExpire = isset(Yii::$app->params['user.accessTokenExpire']) ? (int) Yii::$app->params['user.accessTokenExpire'] : 7200;
                     $accessTokenExpire = $accessTokenExpire ?: 7200;
                     $accessToken = Yii::$app->getSecurity()->generateRandomString() . '.' . (time() + $accessTokenExpire);
-                    Yii::$app->getCache()->set('_access_token', $accessToken);
                     // Update user access_token value
                     $db->createCommand()->update('{{%member}}', ['access_token' => $accessToken], ['id' => $memberId])->execute();
+                    $this->redirect($redirectUrl);
                 } else {
-                    $accessToken = null;
+                    throw new InvalidCallException('微信登录失败。');
                 }
-
-                $this->redirect(urldecode($redirectUrl));
             } else {
                 throw new InvalidCallException('拉取微信认证失败。');
             }
         } else {
-            $this->redirect(urldecode($redirectUrl));
+            $this->redirect($redirectUrl);
         }
     }
 
