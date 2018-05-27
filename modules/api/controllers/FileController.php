19a7c807287f78875264719b511793c48ba24eeb
hiscaler <hiscaler@gmail.com>
2018-05-27 22:43:25 +0800

    api: 重构 file/uploading 接口

--- modules/api/controllers/FileController.php
+++ modules/api/controllers/FileController.php
@@ -5,15 +5,97 @@ namespace app\modules\api\controllers;
 use app\modules\api\extensions\BaseController;
 use yadjet\helpers\StringHelper;
 use Yii;
+use yii\base\DynamicModel;
+use yii\base\InvalidConfigException;
 use yii\helpers\FileHelper;
 use yii\helpers\Url;
 use yii\web\BadRequestHttpException;
 use yii\web\UploadedFile;
 
+/**
+ * 文件处理
+ * Class FileController
+ *
+ * @package app\modules\api\controllers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
 class FileController extends BaseController
 {
 
-    public function actionUploading($key)
+    protected $type = 'file';
+
+    protected function generateUniqueFilename()
+    {
+        return '';
+    }
+
+    /**
+     * 文件上传
+     *
+     * @param string $type
+     * @param string $key
+     * @return array
+     * @throws InvalidConfigException
+     * @throws \yii\base\Exception
+     */
+    public function actionUploading($type = 'image', $key = 'file')
+    {
+        if (!isset(Yii::$app->params['uploading'])) {
+            throw new InvalidConfigException('无效的上传配置。');
+        }
+
+        $config = Yii::$app->params['uploading'];
+        $type = strtolower($type);
+        if (!isset($config[$type])) {
+            throw new InvalidConfigException("无效的 $type 上传配置。");
+        }
+
+        $model = new DynamicModel([$key]);
+        $model->$key = UploadedFile::getInstanceByName($key);
+        $model->addRule($key, $type, [
+            'skipOnEmpty' => false,
+            'enableClientValidation' => false,
+            'extensions' => $config[$type]['extensions'],
+            'minSize' => isset($config[$type]['minSize']) ? $config[$type]['minSize'] : 1024,
+            'maxSize' => isset($config[$type]['maxSize']) ? $config[$type]['maxSize'] : (1024 * 1024 * 200),
+        ]);
+        if ($model->validate()) {
+            /* @var $file \yii\web\UploadedFile */
+            $file = $model->$key;
+            $path = $config['path'];
+            $absolutePath = Yii::getAlias('@web') . $path;
+            !file_exists($absolutePath) && FileHelper::createDirectory($absolutePath);
+            $fileNameHash = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz1234567890'), 0, 5);
+            $filename = sprintf('%s%s.%s', $fileNameHash, date('YmdHis'), $file->getExtension());
+            $success = $file->saveAs($absolutePath . DIRECTORY_SEPARATOR . $filename);
+            if ($success) {
+                $path = "/$path/$filename";
+
+                return [
+                    'originalName' => $file->name,
+                    'realName' => $filename,
+                    'path' => $path,
+                    'fullPath' => Yii::$app->getRequest()->getHostInfo() . $path,
+                    'size' => $file->size,
+                    'type' => $file->getExtension(),
+                ];
+            } else {
+                Yii::$app->getResponse()->setStatusCode(400);
+
+                return [
+                    'message' => '文件上传失败。'
+                ];
+            }
+        } else {
+            Yii::$app->getResponse()->setStatusCode(400);
+
+            return [
+                'message' => $model->getFirstError('file')
+            ];
+        }
+    }
+
+    public function _actionUploading($key)
     {
         $file = UploadedFile::getInstanceByName($key);
         if ($file === null) {
