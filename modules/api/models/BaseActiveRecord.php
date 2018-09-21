b1a20528a7651c801fcc1cc3a50089ef9b541e38
hiscaler <hiscaler@gmail.com>
2018-09-21 10:11:40 +0800

    api: 添加 BaseActiveRecord 基类

---
+++ modules/api/models/BaseActiveRecord.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\modules\api\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Class BaseActiveRecord
+ *
+ * @package app\modules\api\models
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class BaseActiveRecord extends ActiveRecord
+{
+
+    const SCENARIO_DELETE = 'DELETE';
+
+}
