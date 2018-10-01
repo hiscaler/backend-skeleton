c2284ab91fed70083df98af0cc131378a99c116f
hiscaler <hiscaler@gmail.com>
2018-10-01 18:31:14 +0800

    api: 完善 member/index 接口

--- modules/api/controllers/MemberController.php
+++ modules/api/controllers/MemberController.php
@@ -3,7 +3,11 @@
 namespace app\modules\api\controllers;
 
 use app\modules\api\extensions\BaseController;
+use app\modules\api\extensions\UtilsHelper;
 use app\modules\api\models\Member;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\helpers\Inflector;
 use yii\web\NotFoundHttpException;
 
 /**
@@ -16,8 +20,51 @@ use yii\web\NotFoundHttpException;
 class MemberController extends BaseController
 {
 
-    public function actionIndex()
+    public function actionIndex($username = null, $fields = null, $page = 1, $pageSize = 20)
     {
+        // Basic condition
+        $condition = [];
+        $selectColumns = UtilsHelper::filterQuerySelectColumns(['t.id', 't.type', 't.category_id', 'c.name AS category_name', 't.group', 't.username', 't.nickname', 't.real_name', 't.avatar', 't.access_token', 't.email', 't.tel', 't.mobile_phone', 't.address', 't.status', 't.remark', 't.created_at', 't.updated_at', 'u.nickname AS editor'], $fields, []);
+        $query = (new \yii\db\ActiveQuery(Member::class))
+            ->alias('t')
+            ->select($selectColumns);
+        if (in_array('c.name AS category_name', $selectColumns)) {
+            $query->leftJoin('{{%category}} c', '[[t.category_id]] = [[c.id]]');
+        }
+        if (in_array('u.nickname AS editor', $selectColumns)) {
+            $query->leftJoin('{{%user}} u', 't.updated_by = u.id');
+        }
+
+        $query->offset($page)->limit($pageSize);
+
+        $query->where($condition);
+
+        // Order By
+        $orderByColumns = [];
+        if (!empty($orderBy)) {
+            $orderByColumnLimit = ['id', 'usernames', 'categoryId', 'createdAt', 'updatedAt']; // Supported order by column names
+            foreach (explode(',', trim($orderBy)) as $string) {
+                if (!empty($string)) {
+                    $string = explode('.', $string);
+                    if (in_array($string[0], $orderByColumnLimit)) {
+                        $orderByColumns['t.' . Inflector::camel2id($string[0], '_')] = isset($string[1]) && $string[1] == 'asc' ? SORT_ASC : SORT_DESC;
+                    }
+                }
+            }
+        }
+
+        $query->orderBy($orderByColumns ?: ['t.id' => SORT_DESC]);
+        if ($this->debug) {
+            Yii::debug($query->createCommand()->getRawSql(), 'API DEBUG');
+        }
+
+        return new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'page' => (int) $page - 1,
+                'pageSize' => (int) $pageSize ?: 20
+            ]
+        ]);
     }
 
     /**
