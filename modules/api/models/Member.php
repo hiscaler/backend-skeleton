dda910efca5b403c6bc4522bbcaf16894b07e50c
hiscaler <hiscaler@gmail.com>
2018-10-18 08:47:28 +0800

    ...

--- modules/api/models/Member.php
+++ modules/api/models/Member.php
@@ -3,7 +3,6 @@
 namespace app\modules\api\models;
 
 use app\models\Meta;
-use app\modules\api\components\ApplicationHelper;
 use Yii;
 
 class Member extends \app\models\Member
@@ -71,42 +70,6 @@ class Member extends \app\models\Member
                 return $items;
             }
         ];
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @todo 需要处理过期时间
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        /**
-         * Token 格式
-         * 1. token值
-         * 2. token值.有效的时间戳
-         * 3. 类型.token值.有效的时间戳
-         */
-        $member = static::findOne(['access_token' => $token, 'status' => self::STATUS_ACTIVE]);
-        if ($member) {
-            if (stripos($token, '.') === false) {
-                return $member; // 1. token值
-            } else {
-                $tokens = explode('.', $token);
-                if (isset($tokens[2])) {
-                    // 3. 类型.token值.有效的时间戳
-                    list (, , $expire) = $tokens;
-                } else {
-                    // 2. token值.有效的时间戳
-                    list (, $expire) = $tokens;
-                }
-                $accessTokenExpire = ApplicationHelper::getConfigValue('member.accessTokenExpire', 86400);
-                $accessTokenExpire = (int) $accessTokenExpire ?: 86400;
-
-                return ((int) $expire + $accessTokenExpire) > time() ? $member : null;
-            }
-        }
-
-        return null;
     }
 
 }
