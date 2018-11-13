91251fff05d5db7c77f7b5dab6b38a19d0820eed
hiscaler <hiscaler@gmail.com>
2018-11-13 16:01:34 +0800

    admin: 首页只显示七天的登录日志

--- modules/admin/widgets/UserLoginLogs.php
+++ modules/admin/widgets/UserLoginLogs.php
@@ -20,9 +20,21 @@ class UserLoginLogs extends Widget
     {
         $items = [];
         $formatter = Yii::$app->getFormatter();
-        $rawData = Yii::$app->getDb()->createCommand('SELECT [[t.login_ip]], [[t.client_information]], [[t.login_at]] FROM {{%user_login_log}} t WHERE [[t.user_id]] = :userId ORDER BY [[t.login_at]] DESC', [':userId' => Yii::$app->getUser()->getId()])->queryAll();
+        $rawData = Yii::$app->getDb()->createCommand('SELECT [[t.login_ip]], [[t.client_information]], [[t.login_at]] FROM {{%user_login_log}} t WHERE [[t.user_id]] = :userId AND [[t.login_at]] >= :ts ORDER BY [[t.login_at]] DESC', [
+            ':userId' => Yii::$app->getUser()->getId(),
+            ':ts' => strtotime('-2 months')
+        ])->queryAll();
+        $days = 7;
         foreach ($rawData as $data) {
-            $items[$formatter->asDate($data['login_at'])][] = $data;
+            $key = $formatter->asDate($data['login_at']);
+            if (!isset($items[$key])) {
+                $days--;
+                if ($days < 0) {
+                    break;
+                }
+            }
+
+            $items[$key][] = $data;
         }
 
         return $items;
