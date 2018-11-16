bd3a99e5acc8b42d72fc0af53b82c34627d013cd
hiscaler <hiscaler@gmail.com>
2018-11-16 16:40:41 +0800

    ...

--- modules/admin/views/categories/index.php
+++ modules/admin/views/categories/index.php
@@ -4,7 +4,6 @@ use yii\grid\GridView;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\CategorySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = Yii::t('app', 'Categories');
