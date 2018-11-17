c0b0b53a16800b50f0f03ec94178bb0009bbff48
hiscaler <hiscaler@gmail.com>
2018-11-17 23:16:53 +0800

    ...

--- modules/api/modules/article/controllers/DefaultController.php
+++ modules/api/modules/article/controllers/DefaultController.php
@@ -52,6 +52,11 @@ class DefaultController extends Controller
         return $model;
     }
 
+    /**
+     * @param $alias
+     * @return Article|array|null|\yii\db\ActiveRecord
+     * @throws NotFoundHttpException
+     */
     public function findModel($alias)
     {
         $model = Article::find()->where(['alias' => $alias])->one();
@@ -61,4 +66,5 @@ class DefaultController extends Controller
 
         return $model;
     }
+
 }
