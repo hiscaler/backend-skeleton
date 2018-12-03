925c623969b9a39f5b59e651997a607ddd5ca520
hiscaler <hiscaler@gmail.com>
2018-12-03 19:30:20 +0800

    api: ...

--- modules/api/models/Constant.php
+++ modules/api/models/Constant.php
@@ -3,41 +3,12 @@
 namespace app\modules\api\models;
 
 /**
- * 常量定义
+ * Class Constant
  *
- * @package app\models
- *
+ * @package app\modules\api\models
  * @author hiscaler <hiscaler@gmail.com>
  */
-class Constant
+class Constant extends \app\models\Constant
 {
-
-    /**
-     * 布尔值定义
-     */
-    /** 假 */
-    const BOOLEAN_FALSE = 0;
-    /** 真 */
-    const BOOLEAN_TRUE = 1;
-    /**
-     * 状态值定义
-     */
-    /** 待审核 */
-    const STATUS_PENDING = 0;
-    /** 激活 */
-    const STATUS_ACTIVE = 1;
-
-    /**
-     * 未知性别
-     */
-    const SEX_UNKNOWN = 0;
-    /**
-     * 男
-     */
-    const SEX_MALE = 1;
-    /**
-     * 女
-     */
-    const SEX_FEMALE = 2;
 
 }
