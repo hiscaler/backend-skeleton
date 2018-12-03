2d184e4ac6a7ea935dbb309b306c0686fd42b339
hiscaler <hiscaler@gmail.com>
2018-12-03 19:25:56 +0800

    api: 添加 Category 类

---
+++ modules/api/models/Category.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\modules\api\models;
+
+use yii\BaseYii;
+
+/**
+ * Class Category
+ *
+ * @package app\modules\api\models
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Category extends \app\models\Category
+{
+
+    public function fields()
+    {
+        return [
+            'id',
+            'sign',
+            'alias',
+            'name',
+            'shortName' => 'short_name',
+            'parentId' => 'parent_id',
+            'level',
+            'idPath' => 'id_path',
+            'namePath' => 'name_path',
+            'icon' => function ($model) {
+                $icon = $model->icon;
+                if ($icon) {
+                    $icon = \Yii::$app->getRequest()->getHostInfo() . $icon;
+                }
+
+                return $icon;
+            },
+            'description',
+            'enabled' => function ($model) {
+                return $model->enabled ? true : false;
+            },
+            'ordering',
+            'quantity',
+            'createdAt' => 'created_at',
+            'createdBy' => 'created_by',
+            'updatedAt' => 'updated_at',
+            'updatedBy' => 'updated_by',
+        ];
+    }
+
+}
