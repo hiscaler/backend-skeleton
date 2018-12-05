f03e13e57296b8c9585ff72f9886f0c71f43d701
hiscaler <hiscaler@gmail.com>
2018-12-05 21:46:04 +0800

    api: 添加 Lookup 类

---
+++ modules/api/models/Lookup.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\modules\api\models;
+
+/**
+ * Class Lookup
+ *
+ * @package app\modules\api\models
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Lookup extends \app\models\Lookup
+{
+
+    public function fields()
+    {
+        return [
+            'id',
+            'type',
+            'group',
+            'key',
+            'label',
+            'description',
+            'value',
+            'returnType' => 'return_type',
+            'inputMethod' => 'input_method',
+            'inputValue' => 'input_value',
+            'enabled' => function ($model) {
+                return $model->enabled ? true : false;
+            },
+            'createdAt' => 'created_at',
+            'createdBy' => 'created_by',
+            'updatedAt' => 'updated_at',
+            'updatedBy' => 'updated_by',
+        ];
+    }
+
+}
