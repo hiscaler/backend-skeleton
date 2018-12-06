e4d7641346dbd5c943d0666d7f639c986568556e
hiscaler <hiscaler@gmail.com>
2018-12-06 21:49:37 +0800

    api: 添加 Yad 类

---
+++ modules/api/models/Yad.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\modules\api\models;
+
+/**
+ * Class Yad
+ *
+ * @package app\modules\api\models
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Yad extends \app\models\Yad
+{
+
+}
