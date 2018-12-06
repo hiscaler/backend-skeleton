adcc13bfc8ea41c341779c806c24c766330a13a4
hiscaler <hiscaler@gmail.com>
2018-12-06 21:49:22 +0800

    api: 添加 Option 类

---
+++ modules/api/models/Option.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\modules\api\models;
+
+/**
+ * Class Option
+ *
+ * @package app\modules\api\models
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Option extends \app\models\Option
+{
+
+}
