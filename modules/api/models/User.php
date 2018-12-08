c22530f3e861cb19935ed95bc433a7d1f2094292
hiscaler <hiscaler@gmail.com>
2018-12-08 22:19:15 +0800

    api: 添加 User 类

---
+++ modules/api/models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\modules\api\models;
+
+use Yii;
+
+class User extends \app\models\User
+{
+
+    public function fields()
+    {
+        return [
+            'id',
+            'username',
+            'nickname',
+            'avatar' => function () {
+                $avatar = $this->avatar;
+                if (!empty($avatar)) {
+                    $addUrl = true;
+                    foreach (['http', 'https', '//'] as $prefix) {
+                        if (strncasecmp($avatar, $prefix, strlen($prefix)) === 0) {
+                            $addUrl = false;
+                            break;
+                        }
+                    }
+
+                    if ($addUrl) {
+                        $avatar = Yii::$app->getRequest()->hostInfo . $avatar;
+                    }
+                }
+
+                return $avatar;
+            },
+            'email',
+            'role',
+            'registerIp' => 'register_ip',
+            'loginCount' => 'login_count',
+            'lastLoginIp' => 'last_login_ip',
+            'lastLoginTime' => 'last_login_time',
+            'lastLoginSession' => 'last_login_session',
+            'status',
+            'createdAt' => 'created_at',
+            'createdBy' => 'created_by',
+            'updatedAt' => 'updated_at',
+            'updatedBy' => 'updated_by',
+        ];
+    }
+
+}
