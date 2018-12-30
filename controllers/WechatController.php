26f68b3f06d2f184a129d8560746e7472dfeb82c
hiscaler <hiscaler@gmail.com>
2018-12-30 21:51:59 +0800

    ...

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -57,9 +57,10 @@ class WechatController extends Controller
                 if (!$memberId) {
                     $nickname = preg_replace('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $userOriginal["nickname"]);
                     $member = new Member();
-                    $maxId = $db->createCommand('SELECT MAX[[id]] FROM {{%member}}')->queryScalar();
+                    $maxId = $db->createCommand('SELECT MAX([[id]]) FROM {{%member}}')->queryScalar();
                     $member->username = sprintf('wx%08d', $maxId + 1) . rand(1000, 9999);
                     $member->nickname = $nickname ?: $member->username;
+                    $member->real_name = $member->nickname;
                     $member->setPassword($member->username);
                     $member->avatar = $userOriginal['headimgurl'];
                     $member->status = Member::STATUS_ACTIVE;
