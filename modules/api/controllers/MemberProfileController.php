de4903589029857273db7409f7da47ecbc810ca5
hiscaler <hiscaler@gmail.com>
2019-01-08 08:51:29 +0800

    api: 添加 member-pprofile 接口，获取会员相关信息

---
+++ modules/api/controllers/MemberProfileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use app\modules\api\extensions\ActiveController;
+use app\modules\api\models\MemberProfile;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ * Class MemberProfileController
+ *
+ * @package app\modules\api\controllers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class MemberProfileController extends ActiveController
+{
+
+    public $modelClass = MemberProfile::class;
+
+    public function behaviors()
+    {
+        $behaviors = array_merge(parent::behaviors(), [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'create' => ['POST'],
+                    'update' => ['PUT', 'PATCH'],
+                    'delete' => ['POST'],
+                    '*' => ['GET'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index', 'create', 'update', 'view', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+                'denyCallback' => function ($rule, $action) {
+                    throw new \yii\web\ForbiddenHttpException('You are not allowed to access this endpoint');
+                }
+            ],
+        ]);
+
+        return $behaviors;
+    }
+
+}
