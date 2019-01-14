012d62546f56f617fb195158d64e1e8dd89a54d1
hiscaler <hiscaler@gmail.com>
2019-01-14 16:01:42 +0800

    完善前端

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,69 +1,37 @@
 <?php
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use app\assets\AppAsset;
-use app\widgets\Header;
-use yii\helpers\Html;
-use yii\helpers\Url;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Html::csrfMetaTags() ?>        
-        <title><?= Html::encode($this->title) ?></title>
-        <!--[if lt IE 8]><link rel="stylesheet" href="<?php echo Yii::$app->getRequest()->getBaseUrl(); ?>/css/ie.css" type="text/css" media="screen, projection" /><![endif]-->
-        <?php $this->head() ?>
-    </head>
-    <body>
-        <?php $this->beginBody() ?>
-
-        <div id="page-hd">
-            <?= Header::widget() ?>
+<html lang="zh-cmn-Hans">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title><?= $this->title ?> - <?= Yii::$app->name ?></title>
+    <?php $this->head() ?>
+</head>
+<body ontouchstart>
+<?php $this->beginBody() ?>
+<div class="container">
+    <div class="page">
+        <div class="page__hd">
+            <div id="banner"></div>
         </div>
-        <div id="page-bd">
-            <div class="container">
-                <div id="page-left">
-                    <div class="wrapper">
-
-                    </div>
-                    <div class="clearfix">
-                    </div>
-                </div>
-                <div id="page-right">
-                    <div id="innerpage">
-                        <?php
-                        if (isset($this->params['breadcrumbs']) && $this->params['breadcrumbs']) {
-                            echo '<div class="breadcrumbs clearfix">';
-                            echo yii\widgets\Breadcrumbs::widget(
-                                [
-                                    'itemTemplate' => "<li>{link}<i>&raquo;</i></li>",
-                                    'homeLink' => [
-                                        'label' => '首页',
-                                        'url' => Yii::$app->homeUrl,
-                                    ],
-                                    'links' => $this->params['breadcrumbs'],
-                                    'options' => ['class' => 'breadcrumb clearfix']
-                            ]);
-                            echo '</div>';
-                        }
-                        ?>
-                        <div id="innerpage-body">
-                            <?= $content ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="page__bd">
+            <?= $content ?>
         </div>
-        <div id="page-ft">
-            <?= app\widgets\Footer::widget() ?>
+        <div class="page__ft">
+            <?= app\widgets\Tabbar::widget() ?>
         </div>
-        <?php $this->endBody() ?>
-    </body>
+    </div>
+</div>
+<?php $this->endBody() ?>
+</body>
 </html>
 <?php $this->endPage() ?>
--- views/site/error.php
+++ views/site/error.php
@@ -2,18 +2,24 @@
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
+
 /* @var $exception Exception */
 
 use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Yii::t('site', 'Error') ?></h1>
-
-    <div class="message">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="weui-msg">
+    <div class="weui-msg__icon-area"><i class="weui-icon-warn weui-icon_msg"></i></div>
+    <div class="weui-msg__text-area">
+        <h2 class="weui-msg__title"><?= $this->title ?></h2>
+        <p class="weui-msg__desc">
+            <?= nl2br(Html::encode($message)) ?>
+        </p>
     </div>
-
+    <div class="weui-msg__opr-area">
+        <p class="weui-btn-area">
+            <a href="<?= \yii\helpers\Url::toRoute(['/site/index']) ?>" class="weui-btn weui-btn_primary">·µ»ØÊ×Ò³</a>
+        </p>
+    </div>
 </div>
