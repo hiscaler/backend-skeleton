1afda0e0d0ccb15a527f06c9739d8690e1e2f381
hiscaler <hiscaler@gmail.com>
2019-01-25 23:32:49 +0800

    ...

---
+++ forms/MemberRegisterForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\forms;
+
+use app\models\Member;
+use app\models\User;
+use Yii;
+
+/**
+ * 会员注册表单
+ */
+class MemberRegisterForm extends Member
+{
+
+    public $password;
+    public $confirm_password;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            [['password', 'confirm_password', 'email'], 'required'],
+            [['password', 'confirm_password'], 'string', 'min' => 6, 'max' => 12],
+            ['confirm_password', 'compare', 'compareAttribute' => 'password',
+                'message' => '两次输入的密码不一致，请重新输入。'
+            ],
+        ]);
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'username' => Yii::t('site', 'Username'),
+            'password' => Yii::t('site', 'Password'),
+            'confirm_password' => Yii::t('site', 'Confirm password'),
+            'email' => Yii::t('site', 'E-mail'),
+        ];
+    }
+
+    // Events
+    public function beforeValidate()
+    {
+        if (parent::beforeValidate()) {
+            $this->type = self::TYPE_MEMBER;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
