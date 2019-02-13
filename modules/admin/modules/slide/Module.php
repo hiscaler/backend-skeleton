e5324baf4843599ea4644b7301b14edbb33c7e42
hiscaler <hiscaler@gmail.com>
2019-02-13 14:24:33 +0800

    ...

--- modules/admin/modules/slide/Module.php
+++ modules/admin/modules/slide/Module.php
@@ -28,5 +28,5 @@ class Module extends \yii\base\Module
             ],
         ]);
     }
-    
+
 }
--- modules/api/models/MemberProfile.php
+++ modules/api/models/MemberProfile.php
@@ -4,5 +4,5 @@ namespace app\modules\api\models;
 
 class MemberProfile extends BaseMemberProfile
 {
-    
+
 }
