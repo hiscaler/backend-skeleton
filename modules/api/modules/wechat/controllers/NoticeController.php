68d2d56bce7464986ac416a10c7285e97bc9f536
hiscaler <hiscaler@gmail.com>
2019-02-19 09:16:39 +0800

    api: 完善 wechat/notice/send 模版消息发送接口

--- modules/api/modules/wechat/controllers/NoticeController.php
+++ modules/api/modules/wechat/controllers/NoticeController.php
@@ -3,6 +3,8 @@
 namespace app\modules\api\modules\wechat\controllers;
 
 use Yii;
+use yii\filters\VerbFilter;
+use yii\helpers\Json;
 use yii\web\BadRequestHttpException;
 
 /**
@@ -22,24 +24,54 @@ class NoticeController extends BaseController
         $this->service = $this->_application->notice;
     }
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::class,
+            'actions' => [
+                'send' => ['post'],
+            ]
+        ];
+
+        return $behaviors;
+    }
+
     /**
-     * 发送
+     * 发送模板消息
      *
-     * @param $data
      * @return \EasyWeChat\Support\Collection
      * @throws BadRequestHttpException
      */
-    public function actionSend($data)
+    public function actionSend()
     {
-        try {
-            $data = json_decode($data, true);
-            $messageId = $this->service->send($data);
-            $response = Yii::$app->getResponse();
-            $response->setStatusCode(201);
+        $request = Yii::$app->getRequest();
+        $templateId = trim($request->post('templateId'));
+        $openId = trim($request->post('openId'));
+        $url = trim($request->post('url'));
+        $rawData = $request->post('data');
 
-            return $messageId;
-        } catch (\Exception $e) {
-            throw new BadRequestHttpException($e->getMessage());
+        if ($templateId && $openId && $rawData) {
+            try {
+                $data = Json::decode($rawData);
+                if ($data === null) {
+                    throw new BadRequestHttpException('无效的 data 参数。');
+                }
+                $messageId = $this->service->send([
+                    'touser' => trim($openId),
+                    'template_id' => $templateId,
+                    'url' => $url ? urldecode($url) : '',
+                    'data' => $data,
+                ]);
+                $response = Yii::$app->getResponse();
+                $response->setStatusCode(201);
+
+                return $messageId;
+            } catch (\Exception $e) {
+                throw new BadRequestHttpException($e->getMessage());
+            }
+        } else {
+            throw new BadRequestHttpException("参数有误。请提供 templateId, openId, data 参数");
         }
     }
 
