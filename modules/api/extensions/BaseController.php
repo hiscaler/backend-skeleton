2021f3f6a1624dfe5163b7c3cdd5d261f86d5cd6
hiscaler <hiscaler@gmail.com>
2019-02-20 08:45:42 +0800

    ...

--- modules/api/extensions/BaseController.php
+++ modules/api/extensions/BaseController.php
@@ -65,7 +65,7 @@ class BaseController extends Controller
                     'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Max-Age' => 86400, // One day
                 ],
             ],
