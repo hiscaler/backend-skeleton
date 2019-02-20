986726239c0347d9c48f6f848f8b568aba9ef462
hiscaler <hiscaler@gmail.com>
2019-02-20 15:57:30 +0800

    api: Set 'Access-Control-Allow-Credentials' value to false.

--- modules/api/extensions/ActiveController.php
+++ modules/api/extensions/ActiveController.php
@@ -71,7 +71,7 @@ class ActiveController extends \yii\rest\ActiveController
                     'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Max-Age' => 86400, // One day
                 ],
             ],
