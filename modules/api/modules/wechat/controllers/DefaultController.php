9ebf2d63f63377ef96cfd26f559e61f7dbd2522b
hiscaler <hiscaler@gmail.com>
2019-03-16 09:09:37 +0800

    修正获取用户昵称包含特殊字符的问题

--- modules/api/modules/wechat/controllers/DefaultController.php
+++ modules/api/modules/wechat/controllers/DefaultController.php
@@ -81,12 +81,13 @@ class DefaultController extends BaseController
             $wxFieldName = 'openid';
             $wxFieldValue = $openId; // openid
         }
+        $nickname = preg_replace('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $user->nickname);
         $wechatMemberId = $db->createCommand("SELECT [[id]] FROM {{%wechat_member}} WHERE [[$wxFieldName]] = :wxId", [':wxId' => $wxFieldValue])->queryScalar();
         if ($wechatMemberId) {
             $columns = [
                 'openid' => $openId,
                 'subscribe' => Constant::BOOLEAN_TRUE,
-                'nickname' => $user->nickname,
+                'nickname' => $nickname,
                 'sex' => $user->sex,
                 'country' => $user->country,
                 'province' => $user->province,
@@ -102,7 +103,7 @@ class DefaultController extends BaseController
                 'member_id' => 0,
                 'subscribe' => Constant::BOOLEAN_TRUE,
                 'openid' => $openId,
-                'nickname' => $user->nickname,
+                'nickname' => $nickname,
                 'sex' => $user->sex ?: Constant::SEX_UNKNOWN,
                 'country' => $user->country,
                 'province' => $user->province,
