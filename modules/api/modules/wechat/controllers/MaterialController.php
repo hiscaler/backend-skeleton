e44c3228a4734863e97fcfe99e3343c483b3b384
hiscaler <hiscaler@gmail.com>
2019-03-20 16:13:56 +0800

    api: 添加素材管理接口（WIP）

---
+++ modules/api/modules/wechat/controllers/MaterialController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace app\modules\api\modules\wechat\controllers;
+
+use yadjet\helpers\StringHelper;
+use Yii;
+use yii\base\DynamicModel;
+use yii\base\Model;
+use yii\helpers\FileHelper;
+use yii\web\UploadedFile;
+
+/**
+ * 永久素材管理
+ * Class MaterialController
+ *
+ * @property \EasyWeChat\Material\Material $wxService
+ * @package app\modules\api\modules\wechat\controllers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class MaterialController extends BaseController
+{
+
+    /**
+     * 素材类型
+     */
+    const TYPE_IMAGE = 'image';
+    const TYPE_VOICE = 'voice';
+    const TYPE_VIDEO = 'video';
+    const TYPE_THUMB = 'thumb';
+
+    public function init()
+    {
+        parent::init();
+        $this->wxService = $this->wxApplication->material;
+    }
+
+    /**
+     * 文件上传
+     *
+     * @param string $type
+     * @return Model
+     * @throws \yii\base\Exception
+     * @throws \Exception
+     */
+    private function loadModel($type = self::TYPE_IMAGE)
+    {
+        $file = UploadedFile::getInstanceByName('file');
+        $model = new DynamicModel([
+            'path' => null,
+            'file' => $file,
+        ]);
+        $validator = 'file';
+        switch ($type) {
+            case self::TYPE_IMAGE:
+                $options = [
+                    'extensions' => 'bmp,png,jpeg,jpeg,gif',
+                    'minSize' => 1024,
+                    'maxSize' => 1024 * 1024,
+                ];
+                $validator = 'image';
+                break;
+
+            case self::TYPE_VOICE:
+                $options = [
+                    'extensions' => 'mp3,wma,wav,amr',
+                    'minSize' => 1024,
+                    'maxSize' => 1024 * 2018,
+                ];
+                break;
+
+            case self::TYPE_VIDEO:
+                $options = [
+                    'extensions' => 'mp4',
+                    'minSize' => 1024,
+                    'maxSize' => 1024 * 10240,
+                ];
+                break;
+
+            default:
+                $options = [
+                    'extensions' => 'jpg',
+                    'minSize' => 1024,
+                    'maxSize' => 1024 * 64,
+                ];
+                $validator = 'image';
+                break;
+        }
+        $model->addRule('file', $validator, $options);
+        if ($model->validate()) {
+            $dir = Yii::getAlias('@webroot/uploads/wx-materials/' . $type . '/' . date('YmdHis'));
+            if (file_exists($dir)) {
+                FileHelper::createDirectory($dir);
+            }
+            $filename = StringHelper::generateRandomString() . '.' . $file->getExtension();
+            $path = "$dir/$filename";
+            $success = $file->saveAs($path);
+            if ($success) {
+                $model->path = $path;
+            } else {
+                $model->addError('file', '接口端文件存储失败。');
+            }
+        }
+
+        return $model;
+    }
+
+    /**
+     * 永久素材
+     *
+     * @return \EasyWeChat\Material\Material
+     */
+    public function actionIndex()
+    {
+        return $this->wxService;
+    }
+
+    /**
+     * 上传图片
+     *
+     * @throws \yii\base\Exception
+     */
+    public function actionUploadImage()
+    {
+        $model = $this->loadModel(self::TYPE_IMAGE);
+        if ($model->validate()) {
+            return $this->wxService->uploadImage($model->path);
+        } else {
+            return $model;
+        }
+    }
+
+}
