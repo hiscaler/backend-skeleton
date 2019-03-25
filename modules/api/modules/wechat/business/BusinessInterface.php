f8fb88ecc116f7f092d4a5f17fdca819798d9c03
hiscaler <hiscaler@gmail.com>
2019-03-25 11:54:15 +0800

    api: 文档完善

--- modules/api/modules/wechat/business/BusinessInterface.php
+++ modules/api/modules/wechat/business/BusinessInterface.php
@@ -11,6 +11,14 @@ namespace app\modules\api\modules\wechat\business;
 interface BusinessInterface
 {
 
+    /**
+     * 业务逻辑处理代码
+     * 返回 true 表示处理正常，如果处理过程中产生异常的话请返回 false，接口端会根据您的返回结果通知微信支付回调接口。
+     * 返回 false 则会告知微信支付接口业务端逻辑处理失败，支付接口会重新发起回调。
+     *
+     * @param TradeOrder $order
+     * @return bool
+     */
     public function process(TradeOrder $order);
 
 }
