022e7cd929746815824a437978b426d021e540c4
hiscaler <hiscaler@gmail.com>
2019-03-26 17:28:07 +0800

    admin: 完善微信订单搜索

--- modules/admin/modules/wechat/models/OrderSearch.php
+++ modules/admin/modules/wechat/models/OrderSearch.php
@@ -17,8 +17,8 @@ class OrderSearch extends Order
     public function rules()
     {
         return [
-            [['id', 'total_fee', 'time_start', 'time_expire', 'status'], 'integer'],
-            [['appid', 'mch_id', 'device_info', 'nonce_str', 'sign', 'sign_type', 'transaction_id', 'out_trade_no', 'body', 'detail', 'attach', 'fee_type', 'spbill_create_ip', 'goods_tag', 'trade_type', 'product_id', 'limit_pay', 'openid', 'trade_state'], 'safe'],
+            ['status', 'integer'],
+            [['transaction_id', 'out_trade_no', 'trade_state'], 'safe'],
         ];
     }
 
@@ -55,40 +55,13 @@ class OrderSearch extends Order
 
         $this->load($params);
 
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
         // grid filtering conditions
         $query->andFilterWhere([
-            'id' => $this->id,
-            'total_fee' => $this->total_fee,
-            'time_start' => $this->time_start,
-            'time_expire' => $this->time_expire,
             'status' => $this->status,
+            'transaction_id' => $this->transaction_id,
+            'out_trade_no' => $this->out_trade_no,
+            'trade_state' => $this->trade_state,
         ]);
-
-        $query->andFilterWhere(['like', 'appid', $this->appid])
-            ->andFilterWhere(['like', 'mch_id', $this->mch_id])
-            ->andFilterWhere(['like', 'device_info', $this->device_info])
-            ->andFilterWhere(['like', 'nonce_str', $this->nonce_str])
-            ->andFilterWhere(['like', 'sign', $this->sign])
-            ->andFilterWhere(['like', 'sign_type', $this->sign_type])
-            ->andFilterWhere(['like', 'transaction_id', $this->transaction_id])
-            ->andFilterWhere(['like', 'out_trade_no', $this->out_trade_no])
-            ->andFilterWhere(['like', 'body', $this->body])
-            ->andFilterWhere(['like', 'detail', $this->detail])
-            ->andFilterWhere(['like', 'attach', $this->attach])
-            ->andFilterWhere(['like', 'fee_type', $this->fee_type])
-            ->andFilterWhere(['like', 'spbill_create_ip', $this->spbill_create_ip])
-            ->andFilterWhere(['like', 'goods_tag', $this->goods_tag])
-            ->andFilterWhere(['like', 'trade_type', $this->trade_type])
-            ->andFilterWhere(['like', 'product_id', $this->product_id])
-            ->andFilterWhere(['like', 'limit_pay', $this->limit_pay])
-            ->andFilterWhere(['like', 'openid', $this->openid])
-            ->andFilterWhere(['=', 'trade_state', $this->trade_state]);
 
         return $dataProvider;
     }
--- modules/admin/modules/wechat/views/orders/_search.php
+++ modules/admin/modules/wechat/views/orders/_search.php
@@ -7,7 +7,7 @@ use yii\widgets\ActiveForm;
 /* @var $model app\modules\admin\modules\wechat\models\OrderSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-<div class="form-outside form-search form-layout-column" style="display: none">
+<div class="form-outside form-search form-layout-column">
     <div class="order-search form">
         <?php $form = ActiveForm::begin([
             'id' => 'form-orders',
@@ -18,56 +18,15 @@ use yii\widgets\ActiveForm;
             ],
         ]); ?>
         <div class="entry">
-            <?= $form->field($model, 'appid') ?>
-            <?= $form->field($model, 'mch_id') ?>
-        </div>
-        <div class="entry">
             <?= $form->field($model, 'transaction_id') ?>
 
             <?= $form->field($model, 'out_trade_no') ?>
         </div>
         <div class="entry">
             <?= $form->field($model, 'trade_state')->dropDownList(\app\modules\admin\modules\wechat\models\Order::tradeStateOptions(), ['prompt' => '']) ?>
+
+            <?= $form->field($model, 'status')->dropDownList(\app\modules\admin\modules\wechat\models\Order::statusOptions(), ['prompt' => '']) ?>
         </div>
-        <?php // $form->field($model, 'device_info') ?>
-
-        <?php // $form->field($model, 'nonce_str') ?>
-
-        <?php // echo $form->field($model, 'sign') ?>
-
-        <?php // echo $form->field($model, 'sign_type') ?>
-
-        <?php // echo $form->field($model, 'transaction_id') ?>
-
-        <?php // echo $form->field($model, 'out_trade_no') ?>
-
-        <?php // echo $form->field($model, 'body') ?>
-
-        <?php // echo $form->field($model, 'detail') ?>
-
-        <?php // echo $form->field($model, 'attach') ?>
-
-        <?php // echo $form->field($model, 'fee_type') ?>
-
-        <?php // echo $form->field($model, 'total_fee') ?>
-
-        <?php // echo $form->field($model, 'spbill_create_ip') ?>
-
-        <?php // echo $form->field($model, 'time_start') ?>
-
-        <?php // echo $form->field($model, 'time_expire') ?>
-
-        <?php // echo $form->field($model, 'goods_tag') ?>
-
-        <?php // echo $form->field($model, 'trade_type') ?>
-
-        <?php // echo $form->field($model, 'product_id') ?>
-
-        <?php // echo $form->field($model, 'limit_pay') ?>
-
-        <?php // echo $form->field($model, 'openid') ?>
-
-        <?php // echo $form->field($model, 'status') ?>
         <div class="form-group buttons">
             <?= Html::submitButton('搜索', ['class' => 'btn btn-primary']) ?>
             <?= Html::resetButton('重置', ['class' => 'btn btn-default']) ?>
