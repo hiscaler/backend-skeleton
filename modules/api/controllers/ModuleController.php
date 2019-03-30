57af653374685cbc851c94d7745f35bb35e72e27
hiscaler <hiscaler@gmail.com>
2019-03-31 00:08:41 +0800

    api: 添加 module 接口

---
+++ modules/api/controllers/ModuleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use app\modules\api\extensions\ActiveController;
+use app\modules\api\models\Module;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ * 模块管理
+ * Class ModuleController
+ *
+ * @package app\modules\api\controllers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class ModuleController extends ActiveController
+{
+
+    public $modelClass = Module::class;
+
+    public function behaviors()
+    {
+        $behaviors = array_merge(parent::behaviors(), [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'create' => ['POST'],
+                    'update' => ['PUT', 'PATCH'],
+                    'delete' => ['POST'],
+                    '*' => ['GET'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index', 'create', 'update', 'view', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ]);
+
+        return $behaviors;
+    }
+
+}
