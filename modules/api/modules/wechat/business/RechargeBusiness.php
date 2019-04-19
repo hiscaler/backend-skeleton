2d7bb89ceb8a025b3efac630ffeaaa96447882db
hiscaler <hiscaler@gmail.com>
2019-04-19 09:17:16 +0800

    api.webchat: 完善微信充值业务处理

--- modules/api/modules/wechat/business/RechargeBusiness.php
+++ modules/api/modules/wechat/business/RechargeBusiness.php
@@ -6,7 +6,7 @@ use app\modules\api\modules\finance\models\Finance;
 use app\modules\api\modules\wechat\models\Order;
 
 /**
- * 会员充值处理
+ * 会员微信充值处理
  *
  * @package app\modules\api\modules\wechat\business
  * @author hiscaler <hiscaler@gmail.com>
@@ -22,7 +22,13 @@ class RechargeBusiness implements BusinessInterface
         $finance->source = Finance::SOURCE_WECHAT;
         $finance->member_id = $order->member_id;
         $finance->related_key = $order->transaction_id;
-        $finance->remark = trim($order->body . "  " . $order->detail);
+        $finance->status = Finance::STATUS_VALID;
+        $remarks = [];
+        $order->transaction_id && $remarks[] = "微信订单号：{$order->transaction_id}";
+        $order->out_trade_no && $remarks[] = "商户订单号：{$order->out_trade_no}";
+        $order->body && $remarks[] = "　商品描述：{$order->body}";
+        $order->detail && $remarks[] = "　商品详情：{$order->detail}";
+        $finance->remark = implode(PHP_EOL, $remarks);
 
         return $finance->save();
     }
