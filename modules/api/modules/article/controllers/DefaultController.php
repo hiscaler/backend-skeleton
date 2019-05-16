20800f8b06b5c1559b6f30940b71cd639ba09636
hiscaler <hiscaler@gmail.com>
2019-05-16 16:31:35 +0800

    api: 完善 article/view 接口，支持根据 id 或者 alias 进行查询

--- modules/api/modules/article/controllers/DefaultController.php
+++ modules/api/modules/article/controllers/DefaultController.php
@@ -9,12 +9,33 @@ use yii\web\NotFoundHttpException;
 /**
  * /api/article/default
  *
+ * @package app\modules\api\modules\article\controllers
  * @author hiscaler <hiscaler@gmail.com>
  */
 class DefaultController extends ActiveController
 {
 
     public $modelClass = Article::class;
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['view']);
+
+        return $actions;
+    }
+
+    /**
+     * 文章详情
+     *
+     * @param $id
+     * @return Article|array|\yii\db\ActiveRecord|null
+     * @throws NotFoundHttpException
+     */
+    public function actionView($id)
+    {
+        return $this->findModel($id);
+    }
 
     /**
      * @param $id
@@ -26,7 +47,7 @@ class DefaultController extends ActiveController
         if (is_numeric($id)) {
             $condition = ['id' => (int) $id];
         } else {
-            $condition = ['alias' => $id];
+            $condition = ['alias' => trim($id)];
         }
         $model = Article::find()->where($condition)->one();
         if ($model === null) {
