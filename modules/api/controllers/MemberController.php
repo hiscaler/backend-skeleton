5be4b5673f46a2b618a210a3eae57987c4627847
hiscaler <hiscaler@gmail.com>
2019-06-05 09:58:06 +0800

    api: 完善 member 接口

--- modules/api/controllers/MemberController.php
+++ modules/api/controllers/MemberController.php
@@ -5,14 +5,12 @@ namespace app\modules\api\controllers;
 use app\modules\api\extensions\ActiveController;
 use app\modules\api\models\Member;
 use app\modules\api\models\MemberSearch;
-use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
-use yii\web\NotFoundHttpException;
-use yii\web\ServerErrorHttpException;
 
 /**
  * api/member/ 接口
+ * Class MemberController
  *
  * @package app\modules\api\controllers
  * @author hiscaler <hiscaler@gmail.com>
@@ -52,7 +50,6 @@ class MemberController extends ActiveController
     public function actions()
     {
         $actions = parent::actions();
-        unset($actions['view'], $actions['update']);
         $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
 
         return $actions;
@@ -67,49 +64,6 @@ class MemberController extends ActiveController
         $search = new MemberSearch();
 
         return $search->search(\Yii::$app->getRequest()->getQueryParams());
-    }
-
-    /**
-     * @return Member|null
-     * @throws NotFoundHttpException
-     */
-    public function actionView()
-    {
-        return $this->findModel();
-    }
-
-    /**
-     * 更新当前会员资料
-     *
-     * @return mixed
-     * @throws NotFoundHttpException
-     * @throws ServerErrorHttpException
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function actionUpdate()
-    {
-        $model = $this->findModel();
-
-        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
-        if ($model->save() === false && !$model->hasErrors()) {
-            throw new ServerErrorHttpException('Failed to update the object for unknown reason.');
-        }
-
-        return $model;
-    }
-
-    /**
-     * @return Member|null
-     * @throws NotFoundHttpException
-     */
-    protected function findModel()
-    {
-        $model = Member::findOne(\Yii::$app->getUser()->getId());
-        if ($model === null) {
-            throw new NotFoundHttpException("Object not found.");
-        }
-
-        return $model;
     }
 
 }
