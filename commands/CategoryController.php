b0270bd38f1284c6facef85cd65b21bb8cfdf1ac
hiscaler <hiscaler@gmail.com>
2019-07-12 14:34:48 +0800

    console: 完善 category/import 数据导入处理脚本

--- commands/CategoryController.php
+++ commands/CategoryController.php
@@ -6,6 +6,7 @@ use app\models\Category;
 use yadjet\helpers\StringHelper;
 use Yii;
 use yii\console\Exception;
+use yii\helpers\FileHelper;
 
 /**
  * 分类数据处理
@@ -18,32 +19,46 @@ class CategoryController extends Controller
 
     /**
      * 导入分类数据
+     * yii category/import 1 a:aa,b:bb
      *
      * @param $parentId
+     * @param array $replacePairs
      * @throws Exception
      * @throws \yii\db\Exception
      */
-    public function actionImport($parentId)
+    public function actionImport($parentId, array $replacePairs = [])
     {
+        $filePath = FileHelper::normalizePath(__DIR__ . '/data/category.txt');
+        if (!file_exists($filePath)) {
+            throw new Exception(sprintf("`%s` 文件不存在。", $filePath));
+        }
         $this->stdout("Begin..." . PHP_EOL);
+        $pairs = [];
+        foreach ($replacePairs as $item) {
+            if (stripos($item, ":") !== false) {
+                list($k, $v) = explode(":", $item);
+                $k = StringHelper::removeSpace($k);
+                $k && $pairs[$k] = StringHelper::removeSpace($v);
+            }
+        }
         $db = Yii::$app->getDb();
-        $rows = file(__DIR__ . '/data/category.txt');
+        $rows = file($filePath);
+        $pid = null;
+        $name = null;
+        $categoryIdCmd = $db->createCommand('SELECT [[id]] FROM {{%category}} WHERE [[parent_id]] = :parentId AND [[name]] = :name');
         foreach ($rows as $i => $row) {
             $pid = $parentId;
             $row = StringHelper::removeSpace($row);
             $this->stdout("当前正在处理第 " . ($i + 1) . " 条数据 [ $row ]" . PHP_EOL);
-            $names = explode("\t", $row);
-            foreach ($names as $name) {
+            foreach (explode("\t", $row) as $name) {
                 $name = StringHelper::removeSpace($name);
+                $name && $pairs && $name = strtr($name, $pairs);
                 if (!$name) {
                     continue;
                 }
                 $msg = " > $name";
                 $errorMessages = [];
-                $id = $db->createCommand('SELECT [[id]] FROM {{%category}} WHERE [[parent_id]] = :parentId AND [[name]] = :name', [
-                    ':parentId' => $pid,
-                    ':name' => $name
-                ])->queryScalar();
+                $id = $categoryIdCmd->bindValues([':parentId' => $pid, ':name' => $name])->queryScalar();
                 if ($id) {
                     $msg .= " (Nothing)";
                     $pid = $id;
@@ -53,7 +68,15 @@ class CategoryController extends Controller
                     $category->loadDefaultValues();
                     $category->name = $name;
                     $category->parent_id = $pid;
-                    $category->validate() || $category->alias .= mt_rand(0, 10);
+                    if (!$category->validate()) {
+                        foreach ($category->getErrors() as $attr => $error) {
+                            if ($attr == 'alias') {
+                                $n = strlen($category->alias);
+                                $category->alias .= mt_rand($n, $n + 100);
+                                break;
+                            }
+                        }
+                    }
                     if ($category->save()) {
                         $msg .= "(Successful)";
                         $pid = $category->id;
