8897a2994bb0135d2c3101d08dd6c359c4080b76
hiscaler <hiscaler@gmail.com>
2019-07-27 09:45:25 +0800

    Clean

--- helpers/App.php
+++ helpers/App.php
@@ -14,34 +14,6 @@ class App
 {
 
     /**
-     * 判断当前是否在 CLI 模式下
-     *
-     * @return bool
-     */
-    public static function isCli()
-    {
-        if (defined('STDIN')) {
-            return true;
-        }
-
-        if (empty($_SERVER['REMOTE_ADDR']) && !isset($_SERVER['HTTP_USER_AGENT']) && count($_SERVER['argv']) > 0) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * 判断当前是否在非 CLI 模式下
-     *
-     * @return bool
-     */
-    public static function notCli()
-    {
-        return !self::isCli();
-    }
-
-    /**
      * 获取模拟会员 access_token 值，用于后台 api 请求
      *
      * @return false|string|null
