eb5500121de8b9e3e883308da22ab0de4ec2b3e9
hiscaler <hiscaler@gmail.com>
2020-02-25 14:39:36 +0800

    ...

--- spa/src/Greeter.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-};
--- spa/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: {
-        index: __dirname + "/src/index.js"
-    },
-    output: {
-        path: __dirname + "/public",
-        filename: 'bundle.js'
-    }
-}
