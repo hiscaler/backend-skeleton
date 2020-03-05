e083dbe024036e607c5b0ddb6be2e9f572c9eded
hiscaler <hiscaler@gmail.com>
2020-03-05 19:22:46 +0800

    api: 重构 passport./register 接口

--- modules/api/forms/MemberRegisterForm.php
+++ modules/api/forms/MemberRegisterForm.php
@@ -16,9 +16,13 @@ class MemberRegisterForm extends Member
 
     /**
      * 会员注册方式
+     * account. 账号登录：验证用户名和密码
+     * mobile_phone. 手机登录：验证手机号码和密码
+     * captcha. 验证码登录：验证手机号码和验证码
      */
     const REGISTER_TYPE_ACCOUNT = 'account';
     const REGISTER_TYPE_MOBILE_PHONE = 'mobile_phone';
+    const REGISTER_TYPE_CAPTCHA = 'captcha';
 
     /**
      * @var string 注册方式
@@ -55,52 +59,50 @@ class MemberRegisterForm extends Member
             ['register_type', 'string'],
             ['register_type', 'trim'],
             ['register_type', 'default', 'value' => self::REGISTER_TYPE_ACCOUNT],
-            ['register_type', 'in', 'range' => [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE]],
-        ];
-        if ($this->register_type == self::REGISTER_TYPE_ACCOUNT) {
-            $rules = array_merge($rules, [
-                [['password', 'confirm_password'], 'required'],
-                [['password', 'confirm_password'], 'trim'],
-                [['password', 'confirm_password'], 'string', 'min' => 6, 'max' => 30],
-                ['confirm_password', 'compare', 'compareAttribute' => 'password',
-                    'message' => '两次输入的密码不一致，请重新输入。'
-                ],
-            ]);
-        } elseif ($this->register_type == self::REGISTER_TYPE_MOBILE_PHONE) {
-            unset($parentRules['registerByUsername']);
-            $this->username = $this->mobile_phone;
-            $rules = array_merge($rules, [
-                ['captcha', 'required'],
-                ['captcha', 'string'],
-                ['captcha', 'trim'],
-                ['captcha', function ($attribute, $params) {
-                    if (!$this->hasErrors()) {
-                        $cache = Yii::$app->getCache()->get(SmsForm::CACHE_PREFIX . $this->mobile_phone);
-                        if ($cache === false || $cache['expired_datetime'] <= time() || $cache['value'] != $this->captcha) {
-                            $this->addError($attribute, '无效的验证码。');
-                        }
-                    }
+            ['register_type', 'in', 'range' => [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE, self::REGISTER_TYPE_CAPTCHA]],
+            [['password', 'confirm_password'], 'required', 'when' => function ($model) {
+                return in_array($model->register_type, [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE]);
+            }],
+            [['password', 'confirm_password'], 'trim',
+                'when' => function ($model) {
+                    return in_array($model->register_type, [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE]);
                 }],
-            ]);
-        }
-
-        $rules = array_merge($rules, [
-            ['invitation_code', 'string'],
-            ['invitation_code', function ($attribute, $params) {
-                if ($this->invitation_code) {
-                    $memberId = Yii::$app->getDb()->createCommand("SELECT [[id]] FROM {{%member}} WHERE [[unique_key]] = :uniqueKey", [
-                        ':uniqueKey' => $this->invitation_code,
-                    ])->queryScalar();
-                    if ($memberId) {
-                        $this->parent_id = $memberId;
-                    } else {
-                        $this->addError($attribute, '请填写正确的邀请码。');
+            [['password', 'confirm_password'], 'string', 'min' => 6, 'max' => 30,
+                'when' => function ($model) {
+                    return in_array($model->register_type, [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE]);
+                }],
+            ['confirm_password', 'compare', 'compareAttribute' => 'password',
+                'message' => '两次输入的密码不一致，请重新输入。',
+                'when' => function ($model) {
+                    return in_array($model->register_type, [self::REGISTER_TYPE_ACCOUNT, self::REGISTER_TYPE_MOBILE_PHONE]);
+                }],
+            ['captcha', 'required', 'when' => function ($model) {
+                return $model->register_type == self::REGISTER_TYPE_CAPTCHA;
+            }],
+            ['captcha', 'string', 'when' => function ($model) {
+                return $model->register_type == self::REGISTER_TYPE_CAPTCHA;
+            }],
+            ['captcha', 'trim', 'when' => function ($model) {
+                return $model->register_type == self::REGISTER_TYPE_CAPTCHA;
+            }],
+            ['captcha', function ($attribute, $params) {
+                if (!$this->hasErrors()) {
+                    $cache = Yii::$app->getCache()->get(SmsForm::CACHE_PREFIX . $this->mobile_phone);
+                    if ($cache === false || $cache['expired_datetime'] <= time() || $cache['value'] != $this->captcha) {
+                        $this->addError($attribute, '无效的验证码。');
                     }
                 }
+            }, 'when' => function ($model) {
+                return $model->register_type == self::REGISTER_TYPE_CAPTCHA;
             }],
-        ]);
+            ['register_type', function ($model, $params) {
+                if (in_array($this->register_type, [self::REGISTER_TYPE_MOBILE_PHONE, self::REGISTER_TYPE_CAPTCHA])) {
+                    $this->username = $this->mobile_phone;
+                }
+            }],
+        ];
 
-        return array_merge($parentRules, $rules);
+        return array_merge($rules, $parentRules);
     }
 
     public function attributeLabels()
@@ -111,20 +113,6 @@ class MemberRegisterForm extends Member
             'confirm_password' => Yii::t('member', 'Confirm Password'),
             'captcha' => '验证码',
         ]);
-    }
-
-    // Events
-    public function beforeSave($insert)
-    {
-        if (parent::beforeSave($insert)) {
-            if ($this->register_type == self::REGISTER_TYPE_MOBILE_PHONE) {
-                $this->username = $this->mobile_phone;
-            }
-
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
