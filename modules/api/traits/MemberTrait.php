e022cc1d61bcdeb977c4491495a543288699741b
hiscaler <hiscaler@gmail.com>
2020-05-25 14:27:25 +0800

    ...

--- modules/api/traits/MemberTrait.php
+++ modules/api/traits/MemberTrait.php
@@ -80,7 +80,7 @@ trait MemberTrait
         $items = [];
         $rawItems = Yii::$app->getDb()->createCommand('SELECT [[m.key]], [[m.return_value_type]], [[string_value]], [[text_value]], [[integer_value]], [[decimal_value]] FROM {{%meta_value}} t LEFT JOIN {{%meta}} m ON [[t.meta_id]] = [[m.id]] WHERE [[t.object_id]] = :objectId AND [[meta_id]] IN (SELECT [[id]] FROM {{%meta}} WHERE [[table_name]] = :tableName)', [
             ':objectId' => $this->id,
-            ':tableName' => strtr(static::tableName(), ['{{%', '}}' => ''])
+            ':tableName' => str_replace(['{{%', '}}'], '', static::tableName())
         ])->queryAll();
         foreach ($rawItems as $item) {
             $valueKey = Meta::parseReturnKey($item['return_value_type']);
